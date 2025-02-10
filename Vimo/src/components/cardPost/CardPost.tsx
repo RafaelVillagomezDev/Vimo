5c95ae33d77bd6e13ac07667d288f9ac3a1313dd
Rafael Villagomez <102181526+RafaelVillagomezDev@users.noreply.github.com>
2025-02-10 23:53:28 +0100

    Diseño de cards y textos

--- Vimo/src/components/cardPost/CardPost.tsx
+++ Vimo/src/components/cardPost/CardPost.tsx
@@ -1,24 +1,37 @@
 import {
     BoxCard,
+    ButtonOption,
+    ButtonVerMas,
     Card,
     CardBox,
+    CardBoxFlex,
     CardImage,
+    CardOption,
     CardSection,
+    CardSubtitle,
     CardText,
     CardTitle,
+    CardtTextFlex,
     Icon,
     MainCard,
     SectionCard,
 } from './styles/CardPostStyle';
 import Portada_restaurante from '../../assets/pictures/restaurant/Portada_restaurant _1200.jpg';
+import { useState } from 'react';
 
 function CardPost() {
+    const [verMas, setVerMas] = useState(false);
+
+    const handleVerMas = () => setVerMas(true);
+    const handleVerMenos = () => setVerMas(false);
+
     return (
         <MainCard>
             <SectionCard>
-                <BoxCard>
+                <CardOption>
                     <h1>lista</h1>
-                </BoxCard>
+                </CardOption>
+
                 <BoxCard>
                     <Card>
                         <CardSection>
@@ -26,19 +39,80 @@ function CardPost() {
                         </CardSection>
                         <CardSection>
                             <CardBox>
-                                <CardText>Top 1 <Icon>editor_choice</Icon></CardText>
-                                <CardText>
-                                    <CardTitle>
-                                        1. Restaurante Carmen
-                                        
-                                    </CardTitle>
-                                </CardText>
+                                <CardSubtitle>
+                                    Top 1 <Icon>editor_choice</Icon>
+                                </CardSubtitle>
+                                <ButtonOption>Michelin</ButtonOption>
                             </CardBox>
+                            <CardBox>
+                                <CardTitle>1. Restaurante Carmen</CardTitle>
+                            </CardBox>
+                            <CardBox>
+                                <CardtTextFlex>
+                                    <Icon>kid_star</Icon> Puntuación (9,6)
+                                </CardtTextFlex>
+                            </CardBox>
+                            <CardBox>
+                                <CardtTextFlex>
+                                    <Icon>location_on</Icon>C. Santa Barbara, 6, 28609, Madrid
+                                </CardtTextFlex>
+                            </CardBox>
+                            <CardBox>
+                                <CardtTextFlex>
+                                    <Icon>restaurant</Icon>Mediterráneo . Precio medio: 20€
+                                </CardtTextFlex>
+                            </CardBox>
+                            <CardBoxFlex>
+                                <CardText>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit.{' '}
+                                    {verMas &&
+                                        'Consequuntur ipsa corrupti quisquam aperiam amet adipisci, impedit similique maiores nam numquam, commodi ab, odit rem eum saepe ut sed. Deserunt, minus. '}
+                                </CardText>
+                                <ButtonVerMas onClick={verMas ? handleVerMenos : handleVerMas}>
+                                    {verMas ? 'Ver menos' : 'Ver más'}
+                                </ButtonVerMas>
+                            </CardBoxFlex>
                         </CardSection>
                     </Card>
                     <Card>
                         <CardSection>
                             <CardImage src={Portada_restaurante} />
+                        </CardSection>
+                        <CardSection>
+                            <CardBox>
+                                <CardSubtitle>
+                                    Top 1 <Icon>editor_choice</Icon>
+                                </CardSubtitle>
+                                <ButtonOption>Michelin</ButtonOption>
+                            </CardBox>
+                            <CardBox>
+                                <CardTitle>1. Restaurante Carmen</CardTitle>
+                            </CardBox>
+                            <CardBox>
+                                <CardtTextFlex>
+                                    <Icon>kid_star</Icon> Puntuación (9,6)
+                                </CardtTextFlex>
+                            </CardBox>
+                            <CardBox>
+                                <CardtTextFlex>
+                                    <Icon>location_on</Icon>C. Santa Barbara, 6, 28609, Madrid
+                                </CardtTextFlex>
+                            </CardBox>
+                            <CardBox>
+                                <CardtTextFlex>
+                                    <Icon>restaurant</Icon>Mediterráneo . Precio medio: 20€
+                                </CardtTextFlex>
+                            </CardBox>
+                            <CardBoxFlex>
+                                <CardText>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit.{' '}
+                                    {verMas &&
+                                        'Consequuntur ipsa corrupti quisquam aperiam amet adipisci, impedit similique maiores nam numquam, commodi ab, odit rem eum saepe ut sed. Deserunt, minus. '}
+                                </CardText>
+                                <ButtonVerMas onClick={verMas ? handleVerMenos : handleVerMas}>
+                                    {verMas ? 'Ver menos' : 'Ver más'}
+                                </ButtonVerMas>
+                            </CardBoxFlex>
                         </CardSection>
                     </Card>
                 </BoxCard>
--- Vimo/src/components/cardPost/styles/CardPostStyle.tsx
+++ Vimo/src/components/cardPost/styles/CardPostStyle.tsx
@@ -5,38 +5,25 @@ export const MainCard = styled.div`
     margin: 2rem;
 
     @media only screen and (${devices.sm}) {
-        margin: 3rem; 
+        margin: 3rem;
     }
 `;
 
 export const SectionCard = styled.div`
-    display: grid; 
-      
+    display: grid;
     @media only screen and (${devices.sm}) {
-        display: grid; 
-        grid-template-columns:300px auto;
-        gap: 2rem; 
+        display: grid;
+        grid-template-columns: 210px auto;
+        gap: 2rem;
         width: 100%;
     }
-  
-`;
-
-export const LeftColumn = styled.div`
-    display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
-    gap: 1rem;
-`;
-
-export const RightColumn = styled.div`
-    max-width: 300px;
-    width: 100%;
 `;
 
 export const BoxCard = styled.div`
     width: 100%;
-    height: 100%; 
+    height: 100%;
     display: flex;
-    flex-direction: column; 
+    flex-direction: column;
     row-gap: 2rem;
 `;
 
@@ -46,14 +33,32 @@ export const Card = styled.div`
     padding: 1rem;
     box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
     display: flex;
-    flex-direction: row;
+    flex-direction: column;
+    @media only screen and (${devices.sm}) {
+        flex-direction: column;
+    }
+    @media only screen and (${devices.md}) {
+        flex-direction: row;
+    }
+`;
+
+export const CardOption = styled.div`
+    display: none;
+    @media only screen and (${devices.sm}) {
+        display: block;
+    }
 `;
 
 export const CardSection = styled.div`
-    width: 50%;
+    width: 100%;
+    padding: 10px;
+    @media only screen and (${devices.sm}) {
+        width: 100%;
+    }
+    @media only screen and (${devices.md}) {
+        width: 50%;
+    }
 `;
-
-
 
 export const CardImage = styled.img.attrs(({ src }) => ({
     src: src,
@@ -64,26 +69,76 @@ export const CardImage = styled.img.attrs(({ src }) => ({
 `;
 
 export const CardBox = styled.div`
-    padding: 1rem;
+    display: flex;
+    align-items: baseline;
+    padding: 3px;
+    @media only screen and (${devices.sm}) {
+        padding: 0.1rem;
+    }
+`;
+
+export const CardBoxFlex = styled(CardBox)`
+    flex-direction: column;
 `;
 
 export const CardTitle = styled.h3`
-    font-family:'Rokkitt',serif;
-    font-size: 22px;
+    font-family: 'Rokkitt', serif;
+    font-size: 16px;
     font-weight: bold;
+    @media only screen and (${devices.sm}) {
+        font-size: 22px;
+    }
 `;
 
-export const CardText=styled.p`
-   font-family:'Rokkitt',serif;
-   font-size: 20px;
-`
+export const CardSubtitle = styled(CardTitle)`
+    font-weight: 400;
+`;
+
+export const CardText = styled.p`
+    font-family: 'Rokkitt', serif;
+
+    font-size: 12px;
+    padding-bottom: 6px;
+    display: flex;
+    align-items: baseline;
+    @media only screen and (${devices.sm}) {
+        font-size: 20px;
+        padding-bottom: 10px;
+    }
+`;
+
+export const CardtTextFlex = styled(CardText)`
+    display: flex;
+    align-items: end;
+    font-size: 16px;
+    @media only screen and (${devices.sm}) {
+        font-size: 18px;
+    }
+`;
+
+export const ButtonOption = styled.button`
+    padding: 10px 10px;
+    border: none;
+    margin-left: 10px;
+    border-radius: 10px;
+`;
 
 export const Icon = styled.span`
-  font-family: "Material Symbols Outlined";
-  font-size: 22px; 
-  font-variation-settings:
-    "FILL" 1,    
-    "wght" 400,  
-    "GRAD" 0,    
-    "opsz" 24;   
+    font-family: 'Material Symbols Outlined';
+    font-size: 22px;
+    font-variation-settings:
+        'FILL' 1,
+        'wght' 400,
+        'GRAD' 0,
+        'opsz' 24;
+`;
+
+export const ButtonVerMas = styled.button`
+    padding: 12px 10px;
+    border: none;
+    border-radius: 10px;
+    font-size: 10px;
+    background-color: #ff3d00;
+    color: white;
+    font-weight: 400;
 `;
